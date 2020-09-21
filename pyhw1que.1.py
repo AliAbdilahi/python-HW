573b404d9591462ad235a001838161ef119ecb8b
AliAbdilahi <71363632+AliAbdilahi@users.noreply.github.com>
2020-09-21 19:43:04 +0300

    Add files via upload
    Hello sir here are the files for each question

---
+++ pyhw1que.1.py
@@ -0,0 +1,4 @@
+degrees = int(input('Enter Degree:'))
+radians= degrees*3.14/180
+print("Degrees:",degrees)
+print ("radians",radians)
---
+++ pyhw1que2.py
@@ -0,0 +1,9 @@
+student1 = float(input('Enter student1 score:'))
+student2 = float(input('Enter student2 score:'))
+student3 = float(input('Enter student3 score:'))
+Avg= (student1+student2+student3)/3
+print("Student score:")
+print(student1)
+print(student2)
+print(student3)
+print("Average=", Avg)
